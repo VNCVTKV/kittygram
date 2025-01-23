fb179b22ec38c79006d56e41c818be1ddccc9a7d
vncvtkv <cvetkoviv@yandex.ru>
2025-01-23 19:37:54 +0300

    add view class func

--- cats/views.py
+++ cats/views.py
@@ -1,47 +1,14 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from rest_framework import generics
 
 from .models import Cat
 from .serializers import CatSerializer
 
 
-@api_view(['GET', 'POST'])
-def cat_list(request):
-    if request.method == 'POST':
-        serializer = CatSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    cats = Cat.objects.all()
-    serializer = CatSerializer(cats, many=True)
-    return Response(serializer.data)
+class CatList(generics.ListCreateAPIView):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
 
 
-@api_view(['GET', 'DELETE', 'PATCH', 'PUT'])
-def cat_detail(request, pk):
-    try:
-        cat = Cat.objects.get(id=pk)
-    except Cat.DoesNotExist:
-        return Response({"error": "Resource not found"}, status=status.HTTP_404_NOT_FOUND)
-    
-    if request.method == 'DELETE':
-        cat.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-    elif request.method == 'PATCH':
-        serializer = CatSerializer(cat, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'PUT':
-        serializer = CatSerializer(cat, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'GET':
-        serializer = CatSerializer(cat)
-        return Response(serializer.data)
- 
+class CatDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer 
